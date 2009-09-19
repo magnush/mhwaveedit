ad5f3c8a4f3948f28c4c01a2933edb0644d4e56f
Magnus Hjorth <magnus.hjorth@home.se>
2009-09-20 00:19:56 +0200

    recordformatcombo: Use signal_connect_while_alive so the presets_changed callback doesn't get called for deleted combos.

--- src/recordformatcombo.c
+++ src/recordformatcombo.c
@@ -230,22 +230,28 @@ GtkWidget *record_format_combo_new(ListObject *named_presets,
      rfc->named_presets = named_presets;
      gtk_object_ref(GTK_OBJECT(named_presets));
      gtk_object_sink(GTK_OBJECT(named_presets));
-     gtk_signal_connect(GTK_OBJECT(named_presets),"item_added",
-			GTK_SIGNAL_FUNC(presets_changed),rfc);
-     gtk_signal_connect(GTK_OBJECT(named_presets),"item_removed",
-			GTK_SIGNAL_FUNC(presets_changed),rfc);
-     gtk_signal_connect(GTK_OBJECT(named_presets),"item_notify",
-			GTK_SIGNAL_FUNC(presets_changed),rfc);
+     gtk_signal_connect_while_alive(GTK_OBJECT(named_presets),"item_added",
+				    GTK_SIGNAL_FUNC(presets_changed),rfc,
+				    GTK_OBJECT(rfc));
+     gtk_signal_connect_while_alive(GTK_OBJECT(named_presets),"item_removed",
+				    GTK_SIGNAL_FUNC(presets_changed),rfc,
+				    GTK_OBJECT(rfc));
+     gtk_signal_connect_while_alive(GTK_OBJECT(named_presets),"item_notify",
+				    GTK_SIGNAL_FUNC(presets_changed),rfc,
+				    GTK_OBJECT(rfc));
 
      rfc->nameless_presets = nameless_presets;
      gtk_object_ref(GTK_OBJECT(nameless_presets));
      gtk_object_sink(GTK_OBJECT(nameless_presets));
-     gtk_signal_connect(GTK_OBJECT(nameless_presets),"item_added",
-			GTK_SIGNAL_FUNC(presets_changed),rfc);
-     gtk_signal_connect(GTK_OBJECT(nameless_presets),"item_removed",
-			GTK_SIGNAL_FUNC(presets_changed),rfc);
-     gtk_signal_connect(GTK_OBJECT(nameless_presets),"item_notify",
-			GTK_SIGNAL_FUNC(presets_changed),rfc);
+     gtk_signal_connect_while_alive(GTK_OBJECT(nameless_presets),"item_added",
+				    GTK_SIGNAL_FUNC(presets_changed),rfc,
+				    GTK_OBJECT(rfc));
+     gtk_signal_connect_while_alive(GTK_OBJECT(nameless_presets),"item_removed",
+				    GTK_SIGNAL_FUNC(presets_changed),rfc,
+				    GTK_OBJECT(rfc));
+     gtk_signal_connect_while_alive(GTK_OBJECT(nameless_presets),"item_notify",
+				    GTK_SIGNAL_FUNC(presets_changed),rfc,
+				    GTK_OBJECT(rfc));
      rfc->show_other = show_other;
      rfc->current_selection_type = 0;
      rebuild_strings(rfc);
