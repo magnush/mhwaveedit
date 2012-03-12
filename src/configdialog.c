magnush/mhwaveedit

commit 30d862af69ecdec8a606cf5a398b2ca5aafe81f2

Add config option for how to handle Ogg file opening in libsndfile
libsndfile can be built with Vorbis handling support, however this is
slow for random-access. Therefore we provide the choice to either
decompress into a temporary file when opening or to bypass the libsndfile
handling and do decompression via OggDec or Mplayer instead.

src/configdialog.c
@@ -34,6 +34,7 @@
 #include "player.h"
 #include "rateconv.h"
 #include "gettext.h"
+#include "filetypes.h"
 
 static GtkObjectClass *parent_class;
 
@@ -171,6 +172,11 @@ static void config_dialog_ok(GtkButton *button, gpointer user_data)
     inifile_set_guint32("ditherPlayback",
 			dither_playback?DITHER_TRIANGULAR:DITHER_NONE);
     gtk_widget_destroy(GTK_WIDGET(cd));
+
+    if (sndfile_ogg_supported())
+	 inifile_set_guint32("sndfileOggMode",
+			     combo_selected_index(cd->oggmode));
+
     mainwindow_update_texts();
 }
 
@@ -803,6 +809,19 @@ static void config_dialog_init(ConfigDialog *cd)
     gtk_signal_connect(GTK_OBJECT(w),"toggled",
 		       GTK_SIGNAL_FUNC(driver_autodetect_toggled),cd);
 
+    w = combo_new();
+    l = NULL;
+    l = g_list_append(l,_("Direct access"));
+    l = g_list_append(l,_("Decompress"));
+    l = g_list_append(l,_("Bypass"));
+    combo_set_items(COMBO(w),l,inifile_get_guint32("sndfileOggMode",1));
+    g_list_free(l);
+    if (!sndfile_ogg_supported()) {
+	 combo_set_selection(COMBO(w),2);
+	 gtk_widget_set_sensitive(w,FALSE);
+    }
+    cd->oggmode = COMBO(w);
+
     /* Layout the window */
     
     a = gtk_vbox_new(FALSE,5);
@@ -988,8 +1007,17 @@ static void config_dialog_init(ConfigDialog *cd)
     gtk_box_pack_start(GTK_BOX(e),f,FALSE,FALSE,0);
     f = GTK_WIDGET(cd->floating_tempfiles);
     gtk_box_pack_start(GTK_BOX(e),f,FALSE,FALSE,0);
-
-
+    f = gtk_label_new(_("Some versions of libsndfile supports accessing "
+			"Ogg files without decompressing to a temporary "
+			"file."));
+    gtk_box_pack_start(GTK_BOX(e),f,FALSE,FALSE,0);
+    gtk_label_set_line_wrap(GTK_LABEL(f),TRUE);
+    f = gtk_hbox_new(FALSE,3);
+    gtk_box_pack_start(GTK_BOX(e),f,FALSE,FALSE,0);
+    g = gtk_label_new("Libsndfile ogg handling: ");
+    gtk_box_pack_start(GTK_BOX(f),g,FALSE,FALSE,0);
+    g = GTK_WIDGET(cd->oggmode);
+    gtk_box_pack_start(GTK_BOX(f),g,TRUE,TRUE,0);
 
     c = gtk_vbox_new(FALSE,14);
     gtk_container_set_border_width(GTK_CONTAINER(c),8);

src/configdialog.h
@@ -56,6 +56,7 @@ typedef struct {
      GtkEntry *tempdir_add_entry;
      FormatSelector *fallback_format;
      GtkToggleButton *floating_tempfiles;
+     Combo *oggmode;
 } ConfigDialog;
 
 typedef struct {

src/filetypes.c
@@ -75,6 +75,7 @@ static gint sndfile_save(Chunk *chunk, gchar *filename, gpointer settings,
 static gboolean sndfile_save_main(Chunk *chunk, gchar *filename, 
 				  int format, int dither_mode, StatusBar *bar,
 				  gboolean *fatal);
+static gboolean sndfile_ogg_flag = FALSE;
 #endif
 
 static Chunk *raw_load(gchar *filename, int dither_mode, StatusBar *bar);
@@ -163,6 +164,8 @@ static void setup_types(void)
 	  }
 	  if ((info.format&SF_FORMAT_TYPEMASK) == SF_FORMAT_RAW ||
 	      (info.format&SF_FORMAT_TYPEMASK) == SF_FORMAT_WAV) continue;
+	  if ((info.format&SF_FORMAT_TYPEMASK) == SF_FORMAT_OGG)
+	       sndfile_ogg_flag=TRUE;
 	  snprintf(buf,sizeof(buf),".%s",info.extension);
 	  register_file_type((gchar *)info.name, buf, FALSE, sndfile_check, 
 			     sndfile_load, sndfile_save, 
@@ -695,7 +698,9 @@ static gboolean sndfile_check(gchar *filename)
      SNDFILE *s;
      s = sf_open(filename, SFM_READ, &info);
      if (s) sf_close(s);
-     return (s!=NULL && ((info.format&SF_FORMAT_SUBMASK) != SF_FORMAT_RAW));
+     return (s!=NULL && ((info.format&SF_FORMAT_SUBMASK) != SF_FORMAT_RAW)
+	     && ((info.format&SF_FORMAT_SUBMASK) != SF_FORMAT_VORBIS ||
+		 inifile_get_guint32("sndfileOggMode",1)!=2));
 }
 
 static Chunk *sndfile_load(gchar *filename, int dither_mode, StatusBar *bar)
@@ -729,6 +734,12 @@ static Chunk *sndfile_load(gchar *filename, int dither_mode, StatusBar *bar)
      case SF_FORMAT_PCM_16: f.sign=TRUE; f.samplesize=2;break;
 	  /*     case SF_FORMAT_PCM_24: f.sign=TRUE; f.samplesize=3;raw_readable=FALSE;break;
 		 case SF_FORMAT_PCM_32: f.sign=TRUE; f.samplesize=4;raw_readable=FALSE;break; */
+
+     case SF_FORMAT_VORBIS:
+	  if (inifile_get_guint32("sndfileOggMode",1)!=0)
+	       info.seekable = 0;
+	  /* (Fall through on purpose) */
+
 	  /* Default to floating point */	  
      default: f.type = DATAFORMAT_FLOAT; f.samplesize=sizeof(sample_t); break;
      }
@@ -909,6 +920,16 @@ static gint sndfile_save(Chunk *chunk, gchar *filename, gpointer settings,
 }
 
 #endif
+
+gboolean sndfile_ogg_supported(void)
+{
+#if defined(HAVE_LIBSNDFILE)
+     setup_types();
+     return sndfile_ogg_flag;
+#else
+     return FALSE;
+#endif
+}
 
 /* OGG and MP3 */
 

src/filetypes.h
@@ -41,6 +41,9 @@ gchar *fileformat_name(guint fileformat);
 gchar *fileformat_extension(guint fileformat);
 gboolean fileformat_has_options(guint fileformat);
 
+/* Indicate if libsndfile has ogg support */
+gboolean sndfile_ogg_supported();
+
 /* Loads a document from a specified file. */
 
 Chunk *chunk_load(gchar *filename, int dither_mode, StatusBar *bar);

commit d3a1d347139f8bb248d74fde9777d0b1e698c86a

configdialog: Fix read-after-free bug introduced by commit 30d862af69

src/configdialog.c
@@ -171,11 +171,12 @@ static void config_dialog_ok(GtkButton *button, gpointer user_data)
     dither_playback = gtk_toggle_button_get_active(cd->dither_playback);
     inifile_set_guint32("ditherPlayback",
 			dither_playback?DITHER_TRIANGULAR:DITHER_NONE);
-    gtk_widget_destroy(GTK_WIDGET(cd));
 
     if (sndfile_ogg_supported())
 	 inifile_set_guint32("sndfileOggMode",
 			     combo_selected_index(cd->oggmode));
+
+    gtk_widget_destroy(GTK_WIDGET(cd));
 
     mainwindow_update_texts();
 }