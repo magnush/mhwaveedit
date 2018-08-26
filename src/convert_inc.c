magnush/mhwaveedit

commit 8a345d58971cdd0acf78f8fd41c1ecefb59f3f5f

Add support for "unpacked" 24-bit samples using 32 bits/sample

src/convert_inc.c
@@ -91,21 +91,37 @@
 #define C_PCM16UNE_FLOAT C_PCM16UBE_FLOAT
 #define C_PCM32SNE_FLOAT C_PCM32SBE_FLOAT
 #define C_PCM32UNE_FLOAT C_PCM32UBE_FLOAT
+#define C_PCM24SNEPM_FLOAT C_PCM24SBEPM_FLOAT
+#define C_PCM24UNEPM_FLOAT C_PCM24UBEPM_FLOAT
+#define C_PCM24SNEPL_FLOAT C_PCM24SBEPL_FLOAT
+#define C_PCM24UNEPL_FLOAT C_PCM24UBEPL_FLOAT
 
 #define C_PCM16SOE_FLOAT C_PCM16SLE_FLOAT
 #define C_PCM16UOE_FLOAT C_PCM16ULE_FLOAT
 #define C_PCM32SOE_FLOAT C_PCM32SLE_FLOAT
 #define C_PCM32UOE_FLOAT C_PCM32ULE_FLOAT
+#define C_PCM24SOEPM_FLOAT C_PCM24SLEPM_FLOAT
+#define C_PCM24UOEPM_FLOAT C_PCM24ULEPM_FLOAT
+#define C_PCM24SOEPL_FLOAT C_PCM24SLEPL_FLOAT
+#define C_PCM24UOEPL_FLOAT C_PCM24ULEPL_FLOAT
 
 #define C_FLOAT_PCM16SNE C_FLOAT_PCM16SBE
 #define C_FLOAT_PCM16UNE C_FLOAT_PCM16UBE
 #define C_FLOAT_PCM32SNE C_FLOAT_PCM32SBE
 #define C_FLOAT_PCM32UNE C_FLOAT_PCM32UBE
+#define C_FLOAT_PCM24SNEPM C_FLOAT_PCM32SBEPM
+#define C_FLOAT_PCM24UNEPM C_FLOAT_PCM32UBEPM
+#define C_FLOAT_PCM24SNEPL C_FLOAT_PCM32SBEPL
+#define C_FLOAT_PCM24UNEPL C_FLOAT_PCM32UBEPL
 
 #define C_FLOAT_PCM16SOE C_FLOAT_PCM16SLE
 #define C_FLOAT_PCM16UOE C_FLOAT_PCM16ULE
 #define C_FLOAT_PCM32SOE C_FLOAT_PCM32SLE
 #define C_FLOAT_PCM32UOE C_FLOAT_PCM32ULE
+#define C_FLOAT_PCM24SOE C_FLOAT_PCM24SLEPM
+#define C_FLOAT_PCM24UOE C_FLOAT_PCM24ULEPM
+#define C_FLOAT_PCM24SOE C_FLOAT_PCM24SLEPL
+#define C_FLOAT_PCM24UOE C_FLOAT_PCM24ULEPL
 
 static void C_PCM24SLE_FLOAT(guint32 *in, FTYPE *out, int count)
 {
@@ -231,21 +247,37 @@ static void C_PCM24UBE_FLOAT(guint32 *in, FTYPE *out, int count)
 #define C_PCM16UNE_FLOAT C_PCM16ULE_FLOAT
 #define C_PCM32SNE_FLOAT C_PCM32SLE_FLOAT
 #define C_PCM32UNE_FLOAT C_PCM32ULE_FLOAT
+#define C_PCM24SNEPM_FLOAT C_PCM24SLEPM_FLOAT
+#define C_PCM24UNEPM_FLOAT C_PCM24ULEPM_FLOAT
+#define C_PCM24SNEPL_FLOAT C_PCM24SLEPL_FLOAT
+#define C_PCM24UNEPL_FLOAT C_PCM24ULEPL_FLOAT
 
 #define C_PCM16SOE_FLOAT C_PCM16SBE_FLOAT
 #define C_PCM16UOE_FLOAT C_PCM16UBE_FLOAT
 #define C_PCM32SOE_FLOAT C_PCM32SBE_FLOAT
 #define C_PCM32UOE_FLOAT C_PCM32UBE_FLOAT
+#define C_PCM24SOEPM_FLOAT C_PCM24SBEPM_FLOAT
+#define C_PCM24UOEPM_FLOAT C_PCM24UBEPM_FLOAT
+#define C_PCM24SOEPL_FLOAT C_PCM24SBEPL_FLOAT
+#define C_PCM24UOEPL_FLOAT C_PCM24UBEPL_FLOAT
 
 #define C_FLOAT_PCM16SNE C_FLOAT_PCM16SLE
 #define C_FLOAT_PCM16UNE C_FLOAT_PCM16ULE
 #define C_FLOAT_PCM32SNE C_FLOAT_PCM32SLE
 #define C_FLOAT_PCM32UNE C_FLOAT_PCM32ULE
+#define C_FLOAT_PCM24SNEPM C_FLOAT_PCM24SLEPM
+#define C_FLOAT_PCM24UNEPM C_FLOAT_PCM24ULEPM
+#define C_FLOAT_PCM24SNEPL C_FLOAT_PCM24SLEPL
+#define C_FLOAT_PCM24UNEPL C_FLOAT_PCM24ULEPL
 
 #define C_FLOAT_PCM16SOE C_FLOAT_PCM16SBE
 #define C_FLOAT_PCM16UOE C_FLOAT_PCM16UBE
 #define C_FLOAT_PCM32SOE C_FLOAT_PCM32SBE
 #define C_FLOAT_PCM32UOE C_FLOAT_PCM32UBE
+#define C_FLOAT_PCM24SOEPM C_FLOAT_PCM24SBEPM
+#define C_FLOAT_PCM24UOEPM C_FLOAT_PCM24UBEPM
+#define C_FLOAT_PCM24SOEPL C_FLOAT_PCM24SBEPL
+#define C_FLOAT_PCM24UOEPL C_FLOAT_PCM24UBEPL
 
 static void C_PCM24SLE_FLOAT(guint32 *in, FTYPE *out, int count)
 {
@@ -475,6 +507,84 @@ static void C_PCM32UOE_FLOAT(guint32 *in, FTYPE *out, int count)
 #endif
 }
 
+static void C_PCM24SNEPM_FLOAT(guint32 *in, FTYPE *out, int count)
+{
+     unsigned long l;
+     for (; count>0; count--,in++,out++) {
+	  l = (*in >> 8) ^ 0x800000;
+	  *out = NORM24U(l);
+     }
+}
+
+static void C_PCM24SOEPM_FLOAT(guint32 *in, FTYPE *out, int count)
+{
+     guint32 i,x;
+     for (; count>0; count--,in++,out++) {
+	  i = *in;
+	  x = ((i << 16) & 0xFF0000) | (i & 0xFF00) | ((i >> 16) & 0xFF);
+	  x ^= 0x800000;
+	  *out = NORM24U(x);
+     }
+}
+
+static void C_PCM24UNEPM_FLOAT(guint32 *in, FTYPE *out, int count)
+{
+     unsigned long l;
+     for (; count>0; count--,in++,out++) {
+	  l = *in >> 8;
+	  *out = NORM24U(l);
+     }
+}
+
+static void C_PCM24UOEPM_FLOAT(guint32 *in, FTYPE *out, int count)
+{
+     guint32 i,x;
+     for (; count>0; count--,in++,out++) {
+	  i = *in;
+	  x = ((i << 16) & 0xFF0000) | (i & 0xFF00) | ((i >> 16) & 0xFF);
+	  *out = NORM24U(x);
+     }
+}
+
+static void C_PCM24SNEPL_FLOAT(guint32 *in, FTYPE *out, int count)
+{
+     unsigned long l;
+     for (; count>0; count--,in++,out++) {
+	  l = (*in & 0xFFFFFF) ^ 0x800000;
+	  *out = NORM24U(l);
+     }
+}
+
+static void C_PCM24SOEPL_FLOAT(guint32 *in, FTYPE *out, int count)
+{
+     guint32 i,x;
+     for (; count>0; count--,in++,out++) {
+	  i = *in;
+	  x = ((i << 8) & 0xFF0000) | ((i >> 8) & 0xFF00) | ((i >> 24) & 0xFF);
+	  x ^= 0x800000;
+	  *out = NORM24U(x);
+     }
+}
+
+static void C_PCM24UNEPL_FLOAT(guint32 *in, FTYPE *out, int count)
+{
+     unsigned long l;
+     for (; count>0; count--,in++,out++) {
+	  l = *in & 0xFFFFFF;
+	  *out = NORM24U(l);
+     }
+}
+
+static void C_PCM24UOEPL_FLOAT(guint32 *in, FTYPE *out, int count)
+{
+     guint32 i,x;
+     for (; count>0; count--,in++,out++) {
+	  i = *in;
+	  x = ((i << 8) & 0xFF0000) | ((i >> 8) & 0xFF00) | ((i >> 24) & 0xFF);
+	  *out = NORM24U(x);
+     }
+}
+
 static void C_FLOAT_PCM8S(FTYPE *in, signed char *out, int count)
 {
      long int l;
@@ -615,6 +725,73 @@ static void C_FLOAT_PCM32UOE(FTYPE *in, guint32 *out, int count)
      byteswap(out,4,count*4);
 }
 
+static void C_FLOAT_PCM24SNEPM(FTYPE *in, guint32 *out, int count)
+{
+     long l;
+     for (; count>0; count--,in++,out++) {
+	  l = RINT(UNNORM24S(*in));
+	  if (l > 0x7FFFFF) l = 0x7FFFFF;
+	  else if (l < -0x800000) l = -0x800000;
+	  *out = l << 8;
+     }
+}
+
+static void C_FLOAT_PCM24SOEPM(FTYPE *in, guint32 *out, int count)
+{
+     C_FLOAT_PCM24SNEPM(in,out,count);
+     byteswap(out,4,count*4);
+}
+
+static void C_FLOAT_PCM24UNEPM(FTYPE *in, guint32 *out, int count)
+{
+     unsigned long l;
+     for (; count>0; count--,in++,out++) {
+	  l = RINT(UNNORM24U(*in));
+	  if (l > 0xFFFFFF) l = 0xFFFFFF; else if (l < 0) l = 0;
+	  *out = l << 8;
+     }
+}
+
+static void C_FLOAT_PCM24UOEPM(FTYPE *in, guint32 *out, int count)
+{
+     C_FLOAT_PCM24UNEPM(in,out,count);
+     byteswap(out,4,count*4);
+}
+
+static void C_FLOAT_PCM24SNEPL(FTYPE *in, guint32 *out, int count)
+{
+     long l;
+     for (; count>0; count--,in++,out++) {
+	  l = RINT(UNNORM24S(*in));
+	  if (l > 0x7FFFFF) l = 0x7FFFFF;
+	  else if (l < -0x800000) l = -0x800000;
+	  *out = l;
+     }
+}
+
+static void C_FLOAT_PCM24SOEPL(FTYPE *in, guint32 *out, int count)
+{
+     C_FLOAT_PCM24SOEPL(in,out,count);
+     byteswap(out,4,count*4);
+}
+
+static void C_FLOAT_PCM24UNEPL(FTYPE *in, guint32 *out, int count)
+{
+     unsigned long l;
+     for (; count>0; count--,in++,out++) {
+	  l = RINT(UNNORM24U(*in));
+	  if (l > 0xFFFFFF) l = 0xFFFFFF; else if (l < 0) l = 0;
+	  *out = l;
+     }
+}
+
+static void C_FLOAT_PCM24UOEPL(FTYPE *in, guint32 *out, int count)
+{
+     C_FLOAT_PCM24UNEPL(in,out,count);
+     byteswap(out,4,count*4);
+}
+
+
 /* Defined in this file. */
 #undef C_PCM16SNE_FLOAT
 #undef C_PCM16SOE_FLOAT
@@ -624,6 +801,14 @@ static void C_FLOAT_PCM32UOE(FTYPE *in, guint32 *out, int count)
 #undef C_PCM32SOE_FLOAT
 #undef C_PCM32UNE_FLOAT
 #undef C_PCM32UOE_FLOAT
+#undef C_PCM24SNEPM_FLOAT
+#undef C_PCM24SOEPM_FLOAT
+#undef C_PCM24UNEPM_FLOAT
+#undef C_PCM24UOEPM_FLOAT
+#undef C_PCM24SNEPL_FLOAT
+#undef C_PCM24SOEPL_FLOAT
+#undef C_PCM24UNEPL_FLOAT
+#undef C_PCM24UOEPL_FLOAT
 #undef C_FLOAT_PCM16SNE
 #undef C_FLOAT_PCM16SOE
 #undef C_FLOAT_PCM16UNE
@@ -632,6 +817,14 @@ static void C_FLOAT_PCM32UOE(FTYPE *in, guint32 *out, int count)
 #undef C_FLOAT_PCM32SOE
 #undef C_FLOAT_PCM32UNE
 #undef C_FLOAT_PCM32UOE
+#undef C_FLOAT_PCM24SNEPM
+#undef C_FLOAT_PCM24SOEPM
+#undef C_FLOAT_PCM24UNEPM
+#undef C_FLOAT_PCM24UOEPM
+#undef C_FLOAT_PCM24SNEPL
+#undef C_FLOAT_PCM24SOEPL
+#undef C_FLOAT_PCM24UNEPL
+#undef C_FLOAT_PCM24UOEPL
 #undef MUL
 #undef DIV
 #undef MULADD
@@ -665,6 +858,14 @@ static void C_FLOAT_PCM32UOE(FTYPE *in, guint32 *out, int count)
 #undef C_PCM24SBE_FLOAT
 #undef C_PCM24ULE_FLOAT
 #undef C_PCM24UBE_FLOAT
+#undef C_PCM24SLEPM_FLOAT
+#undef C_PCM24SBEPM_FLOAT
+#undef C_PCM24ULEPM_FLOAT
+#undef C_PCM24UBEPM_FLOAT
+#undef C_PCM24SLEPL_FLOAT
+#undef C_PCM24SBEPL_FLOAT
+#undef C_PCM24ULEPL_FLOAT
+#undef C_PCM24UBEPL_FLOAT
 #undef C_PCM32SLE_FLOAT
 #undef C_PCM32SBE_FLOAT
 #undef C_PCM32ULE_FLOAT
@@ -679,6 +880,14 @@ static void C_FLOAT_PCM32UOE(FTYPE *in, guint32 *out, int count)
 #undef C_FLOAT_PCM24SBE
 #undef C_FLOAT_PCM24ULE
 #undef C_FLOAT_PCM24UBE
+#undef C_FLOAT_PCM24SLEPM
+#undef C_FLOAT_PCM24SBEPM
+#undef C_FLOAT_PCM24ULEPM
+#undef C_FLOAT_PCM24UBEPM
+#undef C_FLOAT_PCM24SLEPL
+#undef C_FLOAT_PCM24SBEPL
+#undef C_FLOAT_PCM24ULEPL
+#undef C_FLOAT_PCM24UBEPL
 #undef C_FLOAT_PCM32SLE
 #undef C_FLOAT_PCM32SBE
 #undef C_FLOAT_PCM32ULE

src/chunk.c
@@ -84,6 +84,7 @@ static void chunk_init(Chunk *object)
      object->format.type = DATAFORMAT_PCM;
      object->format.samplerate=44100;
      object->format.samplesize=2;
+     object->format.packing=0;
      object->format.channels=2;
      object->format.sign=1;
      object->format.bigendian = IS_BIGENDIAN;

src/dataformat.h
@@ -36,6 +36,10 @@ typedef struct {
      guint32 samplerate;         /* Samples/sec */
      unsigned samplesize;           /* bytes (PCM: 1,2,3 or 4, 
 				  * float: sizeof(gfloat) or sizeof(gdouble))*/
+     int packing;               /* For non-packed 24-in-32 samples,
+				 * set samplesize to 4 and packing to 1 (data in
+				 * MSB) or 2 (data in LSB)
+				 * For other types, this should be 0 */
      int channels;              /* Number of channels (1-8) */
      int samplebytes;           /* should always be samplesize*channels */
 
@@ -75,7 +79,7 @@ void dataformat_save_to_inifile(gchar *ini_prefix, Dataformat *format,
 				gboolean full);
 
 
-const gchar *sampletype_name(int sampletype, guint samplesize);
+const gchar *sampletype_name(Dataformat *fmt);
 
 
 /* -------------------

src/datasource.c
@@ -51,6 +51,7 @@ static void datasource_init(Datasource *obj)
      obj->format.type = DATAFORMAT_PCM;
      obj->format.samplerate=44100;
      obj->format.samplesize=2;
+     obj->format.packing=0;
      obj->format.channels=2;
      obj->format.sign=1;
      obj->format.bigendian = IS_BIGENDIAN;
@@ -889,7 +890,8 @@ Datasource *datasource_convert(Datasource *source, Dataformat *new_format)
      if (source->format.type == DATAFORMAT_PCM && 
 	 new_format->type == DATAFORMAT_PCM && 
 	 source->format.samplesize == new_format->samplesize &&
-	 source->format.sign == new_format->sign) {	  
+	 source->format.sign == new_format->sign &&
+	 source->format.packing == new_format->packing) {
 	  return datasource_byteswap(source);
      }
 

src/filetypes.c
@@ -518,6 +518,7 @@ static Chunk *wav_load(char *filename, int dither_mode, StatusBar *bar)
      fmt.samplebytes = fmt.samplesize * fmt.channels;
      fmt.sign = (bits>8);
      fmt.bigendian = rifx;
+     fmt.packing = 0;
 
      ds = gtk_type_new(datasource_get_type());
 
@@ -566,10 +567,15 @@ static gboolean wav_save(Chunk *chunk, char *filename, gpointer settings,
 	  /* Check that the sign and endian-ness is correct */
 	  if (fmt->samplesize == 1) q = FALSE;
 	  else q = TRUE;
-	  if (XOR(fmt->sign,q) || fmt->bigendian) {
+	  if (XOR(fmt->sign,q) || fmt->bigendian || fmt->packing!=0) {
 	       memcpy(&cfmt,fmt,sizeof(cfmt));
 	       cfmt.sign = q;
 	       cfmt.bigendian = FALSE;
+	       if (cfmt.packing != 0) {
+		    g_assert(cfmt.samplesize == 4);
+		    cfmt.samplesize = 3;
+		    cfmt.packing = 0;
+	       }
 	       c = chunk_convert(chunk,&cfmt,DITHER_UNSPEC,bar);
 	       b = wav_save(c,filename,settings,type,dither_mode,bar,fatal);
 	       gtk_object_sink(GTK_OBJECT(c));
@@ -723,6 +729,7 @@ static Chunk *sndfile_load(gchar *filename, int dither_mode, StatusBar *bar)
           return NULL;
      }
      /* printf("info.seekable = %d, info.samples = %d\n",info.seekable,info.samples); */
+     memset(&f,0,sizeof(f));
      f.type = DATAFORMAT_PCM;
      f.samplerate = info.samplerate;
      f.channels = info.channels;
@@ -801,6 +808,7 @@ static gboolean find_nearest_sndfile_format(Dataformat *fmt, int format,
      /* Try suggestions first... */
      if (fmt->type == DATAFORMAT_PCM) {
 	  i = fmt->samplesize;
+	  if (fmt->packing != 0) i--;
 	  if (i == 1) i--;
      } else if (fmt->samplesize == sizeof(float)) 
 	  i = 5;
@@ -1035,6 +1043,7 @@ static gboolean ogg_save(Chunk *chunk, gchar *filename, gpointer settings,
 	  memcpy(&fmt,&(chunk->format),sizeof(Dataformat));
 	  fmt.type = DATAFORMAT_PCM;
 	  fmt.samplesize = 4;
+	  fmt.packing = 0;
 	  fmt.samplebytes = fmt.samplesize * fmt.channels;
 	  x = chunk_convert_sampletype(chunk,&fmt);
 	  g_assert(x != NULL);
@@ -1278,7 +1287,7 @@ static Chunk *mp3_load(gchar *filename, int dither_mode, StatusBar *bar)
 static gboolean wav_regular_format(Dataformat *fmt)
 {
      if (fmt->type == DATAFORMAT_FLOAT) return FALSE;
-     return (XOR(fmt->samplesize == 1, fmt->sign));
+     return (XOR(fmt->samplesize == 1, fmt->sign) && fmt->packing == 0);
 }
 
 static gint mp3_save(Chunk *chunk, gchar *filename, gpointer settings,
@@ -1303,9 +1312,10 @@ static gint mp3_save(Chunk *chunk, gchar *filename, gpointer settings,
 				    &clipcount);
      else {
 	  memcpy(&fmt,&(chunk->format),sizeof(Dataformat));
-	  if (fmt.type == DATAFORMAT_FLOAT || fmt.samplesize == 3) {
+	  if (fmt.type == DATAFORMAT_FLOAT || fmt.samplesize == 3 || fmt.packing!=0) {
 	       fmt.type = DATAFORMAT_PCM;
 	       fmt.samplesize = 4;
+	       fmt.packing = 0;
 	  }
 	  fmt.sign = (fmt.samplesize > 1);
 	  fmt.bigendian = FALSE;

src/formatselector.c
@@ -40,6 +40,7 @@ static void samplesize_changed(Combo *obj, gpointer user_data)
      i = combo_selected_index(obj);
      gtk_widget_set_sensitive(GTK_WIDGET(fs->sign_combo),(i<4));
      gtk_widget_set_sensitive(GTK_WIDGET(fs->endian_combo),(i<4));
+     gtk_widget_set_sensitive(GTK_WIDGET(fs->packing_combo),(i==2));
      if (i == 0)
 	  combo_set_selection(fs->sign_combo,0);	  
      else if (i < 4)
@@ -84,6 +85,16 @@ static void format_selector_init(GtkWidget *widget)
      g_list_free(l);
      fs->endian_combo = COMBO(b);
      gtk_table_attach(GTK_TABLE(a),b,1,2,3,4,GTK_FILL,0,0,0);
+     attach_label(_("Alignment:"),a,4,0);
+     b = combo_new();
+     l = g_list_append(NULL,_("Packed"));
+     l = g_list_append(l, _("Top bytes"));
+     l = g_list_append(l, _("Bottom bytes"));
+     combo_set_items(COMBO(b),l,0);
+     g_list_free(l);
+     fs->packing_combo = COMBO(b);
+     gtk_widget_set_sensitive(b, FALSE);
+     gtk_table_attach(GTK_TABLE(a),b,1,2,4,5,GTK_FILL,0,0,0);
      
      fs->channel_combo = NULL;
      fs->rate_box = NULL;
@@ -123,9 +134,9 @@ static void format_selector_show_full(FormatSelector *fs)
      gtk_table_attach(GTK_TABLE(fs),b,1,2,0,1,GTK_FILL,0,0,0);
      fs->channel_combo = COMBO(b);
      gtk_widget_show(b);
-     attach_label(_("Sample rate: "),a,4,0);
+     attach_label(_("Sample rate: "),a,5,0);
      b = gtk_alignment_new(0.0,0.5,0.0,1.0);
-     gtk_table_attach(GTK_TABLE(fs),b,1,2,4,5,GTK_FILL,0,0,0);
+     gtk_table_attach(GTK_TABLE(fs),b,1,2,5,6,GTK_FILL,0,0,0);
      gtk_widget_show(b);
      c = intbox_new(DEFAULT_RATE);
      fs->rate_box = INTBOX(c);
@@ -143,7 +154,11 @@ GtkWidget *format_selector_new(gboolean show_full)
 void format_selector_set(FormatSelector *fs, Dataformat *fmt)
 {
      if (fmt->type == DATAFORMAT_PCM) {
-	  combo_set_selection(fs->samplesize_combo, fmt->samplesize-1);
+	  if (fmt->samplesize == 4 && fmt->packing != 0) {
+	       combo_set_selection(fs->samplesize_combo, 2);
+	       combo_set_selection(fs->packing_combo, fmt->packing);
+	  } else
+	       combo_set_selection(fs->samplesize_combo, fmt->samplesize-1);
 	  combo_set_selection(fs->sign_combo, fmt->sign?1:0);
 	  combo_set_selection(fs->endian_combo, fmt->bigendian?1:0);
      } else {
@@ -164,7 +179,13 @@ void format_selector_get(FormatSelector *fs, Dataformat *result)
      i = combo_selected_index(fs->samplesize_combo);
      if (i<4) {
 	  result->type = DATAFORMAT_PCM;
-	  result->samplesize = i+1;
+	  if (i == 2) {
+	       result->packing = combo_selected_index(fs->packing_combo);
+	       result->samplesize = (result->packing != 0)?4:3;
+	  } else {
+	       result->samplesize = i+1;
+	       result->packing = 0;
+	  }
      } else {
 	  result->type = DATAFORMAT_FLOAT;
 	  result->samplesize = (i>4)?sizeof(double):sizeof(float);

src/formatselector.h
@@ -35,7 +35,7 @@ typedef struct {
 
      GtkTable table;
 
-     Combo *samplesize_combo,*sign_combo,*endian_combo;
+     Combo *samplesize_combo,*sign_combo,*endian_combo,*packing_combo;
      Combo *channel_combo;
      Intbox *rate_box;
 

commit 9c4d61a86599e146bb5ff280658b26cdc94aad70

convert_inc: Fix build failure on big endian architectures.
See https://bugzilla.redhat.com/show_bug.cgi?id=1423942
Bug was introduced in commit 8a345d58971cdd
Fix has been build tested only.

src/convert_inc.c
@@ -108,19 +108,19 @@
 #define C_FLOAT_PCM16UNE C_FLOAT_PCM16UBE
 #define C_FLOAT_PCM32SNE C_FLOAT_PCM32SBE
 #define C_FLOAT_PCM32UNE C_FLOAT_PCM32UBE
-#define C_FLOAT_PCM24SNEPM C_FLOAT_PCM32SBEPM
-#define C_FLOAT_PCM24UNEPM C_FLOAT_PCM32UBEPM
-#define C_FLOAT_PCM24SNEPL C_FLOAT_PCM32SBEPL
-#define C_FLOAT_PCM24UNEPL C_FLOAT_PCM32UBEPL
+#define C_FLOAT_PCM24SNEPM C_FLOAT_PCM24SBEPM
+#define C_FLOAT_PCM24UNEPM C_FLOAT_PCM24UBEPM
+#define C_FLOAT_PCM24SNEPL C_FLOAT_PCM24SBEPL
+#define C_FLOAT_PCM24UNEPL C_FLOAT_PCM24UBEPL
 
 #define C_FLOAT_PCM16SOE C_FLOAT_PCM16SLE
 #define C_FLOAT_PCM16UOE C_FLOAT_PCM16ULE
 #define C_FLOAT_PCM32SOE C_FLOAT_PCM32SLE
 #define C_FLOAT_PCM32UOE C_FLOAT_PCM32ULE
-#define C_FLOAT_PCM24SOE C_FLOAT_PCM24SLEPM
-#define C_FLOAT_PCM24UOE C_FLOAT_PCM24ULEPM
-#define C_FLOAT_PCM24SOE C_FLOAT_PCM24SLEPL
-#define C_FLOAT_PCM24UOE C_FLOAT_PCM24ULEPL
+#define C_FLOAT_PCM24SOEPM C_FLOAT_PCM24SLEPM
+#define C_FLOAT_PCM24UOEPM C_FLOAT_PCM24ULEPM
+#define C_FLOAT_PCM24SOEPL C_FLOAT_PCM24SLEPL
+#define C_FLOAT_PCM24UOEPL C_FLOAT_PCM24ULEPL
 
 static void C_PCM24SLE_FLOAT(guint32 *in, FTYPE *out, int count)
 {