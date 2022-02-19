eef269e535fcb39e62a771dbeb33e15ef4098a50
Marc-Alexandre Espiaut <76531574+malespiaut@users.noreply.github.com>
2022-02-19 11:39:28 +0100

    Adding missing C file, preventing to correctly compile the software.

---
+++ src/help.c
@@ -0,0 +1,25 @@
+/*
+ * Copyright (C) 2004, Magnus Hjorth
+ *
+ * This file is part of mhWaveEdit.
+ *
+ * mhWaveEdit is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by        
+ * the Free Software Foundation; either version 2 of the License, or  
+ * (at your option) any later version.
+ *
+ * mhWaveEdit is distributed in the hope that it will be useful,   
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of  
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with mhWaveEdit; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+
+#include "help.h"
+
+int help_page_count = 0;
+const char *help_page_titles[0] = {0};
+const char **help_page_contents[0] = {0};
